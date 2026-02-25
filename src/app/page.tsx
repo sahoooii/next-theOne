67262b9d155bd4788da0882d20ddbb44698283a3
sahoooii <saho.chu@gmail.com>
2026-02-25 18:16:55 +0900

    Installed react-icons and framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 'use client';
 
 import { Button } from '@heroui/react';
+import Link from 'next/link';
+import { FaRegSmile } from 'react-icons/fa';
 
 export default function Home() {
 	return (
 		<div className='p-10 space-y-4'>
 			<h1 className='text-3xl font-bold'>HeroUI Test</h1>
-			<Button color='primary'>Click Me</Button>
+			<Button
+				as={Link}
+				href='/members'
+				color='primary'
+				variant='bordered'
+				startContent={<FaRegSmile size={20} />}
+			>
+				Click Me
+			</Button>
 		</div>
 	);
 }
