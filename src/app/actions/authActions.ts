3dfaf5253d9047922f01c85ff4ccb647c0ec931e
sahoooii <saho.chu@gmail.com>
2026-03-26 18:26:45 +0900

    Add error handling at register form

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -2,41 +2,49 @@
 
 import { prisma } from '@/lib/prisma';
 import { registerSchema, RegisterSchema } from '@/lib/schema/registerForm';
+import { ActionResult } from '@/types';
+import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
-export async function registerUser(data: RegisterSchema) {
-	const validated = registerSchema.safeParse(data);
+export async function registerUser(
+	data: RegisterSchema,
+): Promise<ActionResult<User>> {
+	try {
+		const validated = registerSchema.safeParse(data);
 
-	// Fix easy to use in FE
-	if (!validated.success) {
-		const fieldErrors = validated.error.issues.reduce(
-			(acc, issue) => {
-				const field = issue.path[0] as string;
-				acc[field] = issue.message;
-				return acc;
+		if (!validated.success) {
+			const fieldErrors = validated.error.issues.reduce(
+				(acc, issue) => {
+					const field = issue.path[0] as string;
+					acc[field] = issue.message;
+					return acc;
+				},
+				{} as Record<string, string>,
+			);
+			return { status: 'error', error: fieldErrors };
+		}
+		const { name, email, password } = validated.data;
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// Check a user already registered
+		const existingUser = await prisma.user.findUnique({
+			where: { email },
+		});
+
+		if (existingUser) return { status: 'error', error: 'User already exists' };
+
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email,
+				passwordHash: hashedPassword,
 			},
-			{} as Record<string, string>,
-		);
+		});
 
-		return { error: fieldErrors };
+		return { status: 'success', data: user };
+	} catch (error) {
+		console.log(error);
+		return { status: 'error', error: 'Something went wrong' };
 	}
-
-	const { name, email, password } = validated.data;
-
-	const hashedPassword = await bcrypt.hash(password, 10);
-
-	// Check a user already registered
-	const existingUser = await prisma.user.findUnique({
-		where: { email },
-	});
-
-	if (existingUser) return { error: 'User already exists' };
-
-	return prisma.user.create({
-		data: {
-			name,
-			email,
-			passwordHash: hashedPassword,
-		},
-	});
 }
