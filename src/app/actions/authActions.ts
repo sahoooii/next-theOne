d4ae42880fb5eb3034054e6cba1a51916b097574
sahoooii <saho.chu@gmail.com>
2026-03-27 16:47:33 +0900

    Created login action

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,10 +1,40 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { prisma } from '@/lib/prisma';
+import { LoginSchema } from '@/lib/schema/loginSchema';
 import { registerSchema, RegisterSchema } from '@/lib/schema/registerForm';
 import { ActionResult } from '@/types';
 import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
+import { AuthError } from 'next-auth';
+
+export async function signInUser(
+	data: LoginSchema,
+): Promise<ActionResult<string>> {
+	try {
+		const result = await signIn('credentials', {
+			email: data.email,
+			password: data.password,
+			redirect: false,
+		});
+		console.log(result);
+		return { status: 'success', data: 'Logged In' };
+	} catch (error) {
+		console.log(error);
+
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return { status: 'error', error: 'Invalid credentials' };
+				default:
+					return { status: 'error', error: 'Something went wrong' };
+			}
+		} else {
+			return { status: 'error', error: 'Something went wrong' };
+		}
+	}
+}
 
 export async function registerUser(
 	data: RegisterSchema,
@@ -47,4 +77,12 @@ export async function registerUser(
 		console.log(error);
 		return { status: 'error', error: 'Something went wrong' };
 	}
+}
+
+export async function getUserByEmail(email: string) {
+	return prisma.user.findUnique({ where: { email } });
+}
+
+export async function getUserById(id: string) {
+	return prisma.user.findUnique({ where: { id } });
 }
