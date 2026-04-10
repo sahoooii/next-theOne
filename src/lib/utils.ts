34c4c958d7626a8c33874d6339f5571f9da127b3
sahoooii <saho.chu@gmail.com>
2026-04-10 18:39:34 +0900

    Add members details link and use date-fns for calculate dob

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+import { differenceInYears } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+// dob→ Date of birth
+export function calculateAge(dob: Date) {
+	return differenceInYears(new Date(), dob);
 }
