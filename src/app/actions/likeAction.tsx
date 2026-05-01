e82996540e305d2e76e19653612e8a5bbbeb2bef
sahoooii <saho.chu@gmail.com>
2026-05-01 17:08:29 +0900

    Created like toggle and get likesIds action

---
+++ src/app/actions/likeAction.tsx
@@ -0,0 +1,53 @@
+'use server';
+
+import { auth } from '@/auth';
+import { prisma } from '@/lib/prisma';
+import { getAuthUserId } from './authActions';
+
+export async function toggleLikeMember(targetUserId: string, isLiked: boolean) {
+	try {
+		const userId = await getAuthUserId();
+		// if already liked, delete the like
+		if (isLiked) {
+			await prisma.like.delete({
+				where: {
+					// 複合キーは「まとめて1つのIDだけど、その中身は2つ必要
+					sourceUserId_targetUserId: {
+						sourceUserId: userId,
+						targetUserId,
+					},
+				},
+			});
+		} else {
+			await prisma.like.create({
+				data: {
+					sourceUserId: userId,
+					targetUserId,
+				},
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
+
+export async function fetchCurrentUserLikeIds() {
+	try {
+		const userId = await getAuthUserId();
+
+		const likeIds = await prisma.like.findMany({
+			where: {
+				sourceUserId: userId,
+			},
+			select: {
+				targetUserId: true,
+			},
+		});
+
+		return likeIds.map((like) => like.targetUserId);
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
