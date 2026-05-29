6bfe823c8fa170bc2753385b366010eb7a75f993
sahoooii <saho.chu@gmail.com>
2026-05-29 23:37:29 +0900

    Add avatar and date and time function at chat page. Fixed mapping function of date

--- src/app/members/[userId]/loading.tsx
+++ src/app/members/[userId]/loading.tsx
@@ -1,7 +1,7 @@
 import { LoadingDisplay } from '@/components/LoadingDisplay';
 
 const Loading = () => {
-	return <LoadingDisplay message='Loading profile...' />;
+	return <LoadingDisplay message='Loading user data...' />;
 };
 
 export default Loading;
