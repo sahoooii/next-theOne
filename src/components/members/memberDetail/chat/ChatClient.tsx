28e541b92a4c78660077ebd7d1e6f5cc22fee085
sahoooii <saho.chu@gmail.com>
2026-06-01 15:39:05 +0900

    Created chat skeleton for chat page

---
+++ src/components/members/memberDetail/chat/ChatClient.tsx
@@ -0,0 +1,31 @@
+import { Card } from '@/components/ui/card';
+import MemberDetailPageHeader from '@/components/members/memberDetail/MemberDetailPageHeader';
+import ChatForm from './ChatForm';
+import { MessageDto } from '@/types';
+
+type Props = {
+	messages: MessageDto[];
+	currentUserId: string;
+}
+
+
+const ChatClient = ({messages, currentUserId}: Props) => {
+	return (
+		<Card
+			className='
+	h-full
+	bg-white/70 backdrop-blur-md
+	border border-black/10
+	rounded-2xl
+	p-8
+'
+		>
+			{/* Header */}
+			<MemberDetailPageHeader title='Chat' />
+			{/* Chat contents */}
+			<ChatForm messages={messages} currentUserId={currentUserId} />
+		</Card>
+	);
+}
+
+export default ChatClient
