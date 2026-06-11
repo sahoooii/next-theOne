7031e28161eb1268446b4cd0ed7e1e40e0d0d86d
sahoooii <saho.chu@gmail.com>
2026-06-11 15:36:43 +0900

    Add /* eslint-disable @typescript-eslint/no-unused-vars */ for deploy temporary

--- src/components/profile/MemberProfileForm.tsx
+++ src/components/profile/MemberProfileForm.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Member } from '@prisma/client';
 
 type Props = {
 	member?: Member;
 	mode: 'create' | 'edit';
 };
 
-const MemberProfileForm = ({member, mode}: Props) => {
-	return (
-		<div>MemberProfileForm</div>
-	)
-}
+const MemberProfileForm = ({ member, mode }: Props) => {
+	return <div>MemberProfileForm</div>;
+};
 
-export default MemberProfileForm
+export default MemberProfileForm;
