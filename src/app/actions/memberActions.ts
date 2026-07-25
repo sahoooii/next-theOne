80610e139f1d773fe25dadf9195001853214beb4
sahoooii <saho.chu@gmail.com>
2026-07-25 19:01:28 +0900

    refactor(chat): introduce ChatPartner model for chat UI

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -2,7 +2,9 @@
 
 import { prisma } from '@/lib/prisma';
 import { getAuthUserId } from './authActions';
+import { ChatPartner } from '@/types/prisma';
 
+// Display all users exclude current login user
 export async function getMembers() {
 	const userId = await getAuthUserId();
 
@@ -41,4 +43,23 @@ export async function getMemberPhotoByUserId(userId: string) {
 	if (!member) return null;
 
 	return member.photos;
+}
+
+// For chat room: Chat partner info
+export async function getChatPartner(
+	userId: string,
+): Promise<ChatPartner | null> {
+	try {
+		return prisma.member.findUnique({
+			where: { userId },
+			select: {
+				userId: true,
+				name: true,
+				image: true,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 }
