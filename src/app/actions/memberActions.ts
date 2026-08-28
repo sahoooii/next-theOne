fd3744bb95cd8b34416b8f85e07af7460d8afde4
sahoooii <saho.chu@gmail.com>
2026-08-28 16:00:15 +0900

    Add Candidate discoverable logic based on user gender and search gender

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -1,20 +1,61 @@
 'use server';
 
 import { prisma } from '@/lib/prisma';
-import { getAuthUserId } from './authActions';
 import { ChatPartner } from '@/types/prisma';
 
-// Display all users exclude current login user
+import { getAuthUserId } from './authActions';
+import { SearchGender } from '@prisma/client';
+
+// Candidate is discoverable when:
+// 1. Candidate is not myself
+// 2. My searchGender matches candidate.gender
+//    OR my searchGender is ANY
+// 3. Candidate's searchGender matches my gender
+//    OR candidate's searchGender is ANY
 export async function getMembers() {
 	const userId = await getAuthUserId();
 
 	try {
+		const currentMember = await prisma.member.findUnique({
+			where: {
+				userId,
+			},
+			select: {
+				gender: true,
+				searchGender: true,
+			},
+		});
+
+		if (!currentMember) {
+			throw new Error('Current member not found');
+		}
+
+		// 自分が相手に求めている条件
+		const genderFilter =
+			currentMember.searchGender === SearchGender.ANY
+				? {} //私は誰でも探すので、相手の gender では絞らない
+				: { gender: currentMember.searchGender };
+
+		// 相手も自分を探しているか
+		const searchGenderFilter = {
+			OR: [
+				{
+					searchGender: currentMember.gender as SearchGender,
+				},
+				{
+					searchGender: SearchGender.ANY,
+				},
+			],
+		};
+
 		return prisma.member.findMany({
 			// Exclude login user
 			where: {
 				NOT: {
 					userId: userId,
 				},
+				...genderFilter,
+				...searchGenderFilter,
 			},
 		});
 	} catch (error) {
