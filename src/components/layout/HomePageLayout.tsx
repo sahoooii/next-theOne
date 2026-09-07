d1f166878ca0cb95f39591b26dfec26c6c29c77d
sahoooii <saho.chu@gmail.com>
2026-09-07 16:38:31 +0900

    Created: Home CTA

--- src/components/layout/HomePageLayout.tsx
+++ src/components/layout/HomePageLayout.tsx
@@ -1,6 +1,6 @@
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<main className='min-h-[calc(100vh-80px)] pb-36 lg:pb-24'>{children}</main>
+		<main className='min-h-[calc(100vh-80px)]'>{children}</main>
 	);
 };
 
